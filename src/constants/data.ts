f5e80bad069699b5d92e8cb527b554947c91c2d0
iamparmjeet <iamparmjeetmishra@gmail.com>
2026-02-28 22:19:09 +0530

    Update=>Socials and contact info added

--- src/constants/data.ts
+++ src/constants/data.ts
@@ -1,3 +1,95 @@
+import {
+	IconBrandFacebook,
+	IconBrandInstagram,
+	IconBrandWhatsapp,
+	IconBrandYoutube,
+	IconMail,
+	IconMapPin,
+	IconPhone,
+	IconShoppingCart,
+	type TablerIcon,
+} from "tabler-icons";
+
 export const media = "https://media.krmenggworks.com/assets";
 export const favicon = `${media}/06/cropped-KRM-Favicon.jpg`;
 export const logo = `${media}/03/KRM-Logo-Transparent.png`;
+
+export type ContactItemType = {
+	id: number;
+	label: string;
+	link: string;
+	icon: TablerIcon;
+};
+
+export const ContactInfo: ContactItemType[] = [
+	{
+		id: 1,
+		label: "+91 86045 07464",
+		link: "tel:+918604507464",
+		icon: IconPhone,
+	},
+	{
+		id: 2,
+		label: "+91 86010 33282",
+		link: "tel:+918601033282",
+		icon: IconPhone,
+	},
+	{
+		id: 3,
+		label: "info@krmenggworks.com",
+		link: "mailto:info@krmenggworks.com",
+		icon: IconMail,
+	},
+	{
+		id: 4,
+		label: "krmenggworks@gmail.com",
+		link: "mailto:krmenggworks@gmail.com",
+		icon: IconMail,
+	},
+	{
+		id: 5,
+		label: "Deosth Deoria, Deoria, Uttar Pradesh",
+		link: "https://www.google.com/maps/place/Karmullah+Engineering+Works/@26.6566447,83.8199664,17z/data=!3m1!4b1!4m6!3m5!1s0x3993c35d34370ec7:0xd3e4b20697f81b45!8m2!3d26.6566399!4d83.8225413!16s%2Fg%2F11trzmqtyh?entry=ttu",
+		icon: IconMapPin,
+	},
+];
+
+export type SocialsItemType = {
+	id: number;
+	label: string;
+	link: string;
+	icon: TablerIcon;
+};
+
+export const Socials: SocialsItemType[] = [
+	{
+		id: 1,
+		label: "Insta",
+		link: "https://www.instagram.com/krmenggworks",
+		icon: IconBrandInstagram,
+	},
+	{
+		id: 2,
+		label: "FB",
+		link: "https://www.facebook.com/profile.php?id=61557026939235&mibextid=ZbWKwL",
+		icon: IconBrandFacebook,
+	},
+	{
+		id: 3,
+		label: "Instamart",
+		link: "https://indiamart.com/karmullah-engineering-works/",
+		icon: IconShoppingCart,
+	},
+	{
+		id: 4,
+		label: "Youtube",
+		link: "https://www.youtube.com/channel/UCsIHla4hLleijxEKx5_EEKQ",
+		icon: IconBrandYoutube,
+	},
+	{
+		id: 5,
+		label: "Whatsapp",
+		link: "https://api.whatsapp.com/send?phone=",
+		icon: IconBrandWhatsapp,
+	},
+];
