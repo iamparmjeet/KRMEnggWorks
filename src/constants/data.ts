5e653acbe078678466cb710d14a921a4b3b4bb0b
iamparmjeet <iamparmjeetmishra@gmail.com>
2026-02-28 20:10:59 +0530

    Update=>media data extracted

---
+++ src/constants/data.ts
@@ -0,0 +1,3 @@
+export const media = "https://media.krmenggworks.com/assets";
+export const favicon = `${media}/06/cropped-KRM-Favicon.jpg`;
+export const logo = `${media}/03/KRM-Logo-Transparent.png`;
